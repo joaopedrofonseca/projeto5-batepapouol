0ded5e1c9189db279c685025b0b8c579e3fc348c
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2022-10-28 12:36:43 -0300

    Bate-Papo UOL  v.sem rolagem automática

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ function username(){
     promise.catch(fail);
 }
 username();
+
 function fiveSecConection(){
     objName = {name: userName};
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objName);
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", objName);
     promise.then(success);
     promise.catch(fail);
 }
 
-//setTimeout(fiveSecConection, 5000);
+setInterval(fiveSecConection, 5000);
 
 function success(warning){
     console.log(warning.status);
 }
 
 function fail(warning){
-   // if(warning.response.status === 400){
-    //    window.location.reload();
-   // }
+    if(warning.response.status === 400){
+       window.location.reload();
+    }
 }
-let msg = [];
+
 
 function getMsg(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -40,7 +41,11 @@ setInterval(getMsg, 3000);
 function answr(resp){
     msg = resp.data;
     renderizeMsg();
+    setInterval(scrollmesssage, 3000);
 }
+
+let msg = [];
+let lastmsg;
 function renderizeMsg(){
     const messages = document.querySelector('.screenMessages');
     messages.innerHTML = '';
@@ -64,6 +69,7 @@ function renderizeMsg(){
                 <div class="msg"> ${text}</div>
             </div>
             `
+
         } else {
             if(from === userName){
             messages.innerHTML = messages.innerHTML +`
@@ -74,6 +80,7 @@ function renderizeMsg(){
 
             </div>
             `
+
         }else{
             messages.innerHTML = messages.innerHTML +`
             <div class="text privateMsg">
@@ -82,10 +89,22 @@ function renderizeMsg(){
 
             </div>
             `
+
         }
         }
     }
 }
+
+
+function scrollmesssage(){
+    lastmsg = msg;
+    if(lastmsg[99] !== msg[99]){
+        let element = document.querySelectorAll(".text");
+        element[(element.length - 1)].scrollIntoView({behavior: "smooth"});
+    }
+}
+
+
 
 let userMsg = {};
 function sendMessage(){
@@ -94,4 +113,10 @@ function sendMessage(){
     let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', userMsg);
     promise.then(success);
     promise.catch(fail);
+    cleanText();
+}
+
+function cleanText(){
+    const u = document.querySelector(".message textarea");
+    u.value="";
 }
